86fdc21055f6bbceb8ff0853d6228e5c802caaef
Muhammad Rido <muhammadrido19@gmail.com>
2023-06-12 21:58:48 +0700

    remove unuse code

--- src/pages/about/contact/contact.js
+++ src/pages/about/contact/contact.js
@@ -1,22 +1,9 @@
 import React, { Component } from 'react'
-import emailIcon from '../../../assets/images/icons/gmail.svg'
-import linkedinIcon from '../../../assets/images/icons/linkedin.svg'
-import githubIcon from '../../../assets/images/icons/github.svg'
-import instagramIcon from '../../../assets/images/icons/instagram.svg'
 import ContactComponent from './contactComponent'
 import JSONData from '../../../assets/json/contact.json'
 import env from '../../../assets/json/global.json'
 
 class Contact extends Component {
-    state = {
-        icon: [
-            {name: linkedinIcon, link: 'https://linkedin.com/in/muhammad-rido-533953140', toolTip: 'LinkedIn: Muhammad Rido'},
-            {name: emailIcon, link: 'mailto:muhammadrido19@gmail.com', toolTip: 'gmail: muhammadrido19@gmail.com'},
-            {name: githubIcon, link: 'https://github.com/mrido10/', toolTip: 'GitHub: mrido10'},
-            {name: instagramIcon, link: 'https://www.instagram.com/m.rido10/', toolTip: 'Instagram: @mrido10'}
-        ]
-    }
-
     render() {
         return(
             <section id='contactCont' className='cont1 contact'>
